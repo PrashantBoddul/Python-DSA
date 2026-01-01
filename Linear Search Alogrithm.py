7cb88f24974fb2c556da10e0faf1d65ab7a2f73f
Prashant Boddul <boddulprashant@gmail.com>
2026-01-01 12:15:22 +0530

    Add files via upload

--- Linear Search Alogrithm.py
+++ Linear Search Alogrithm.py
@@ -4,7 +4,7 @@ print('Cards:-', Cards)
 
 print('\nSolve The DSA : - We have 13 card at which position card is 5 you have to check by turing minimum cards\n')
 
-CheckCard=int(input("Enter Card Which You Want To Test :-\t"))
+CheckCard=str(input("Enter Card Which You Want To Test :-\t"))
 #We have 13 card at which position card is 5 you have to check by turing minimum cards
 
 NoOfCard=len(Cards)
@@ -15,13 +15,14 @@ NoOfCardTurned=''
 for cardno in range(0,NoOfCard,1):
     #print(Cards[cardno])
     CheckedCardList.append(cardno)
-    if Cards[cardno]==CheckCard:
+    print(Cards[cardno],CheckCard)
+    if str(Cards[cardno])==CheckCard:        
         NoOfCardTurned=len(CheckedCardList)
         PostionOfTheCard=cardno
         print('\nNo OF Cards Turned By Using Linear Search Algorithm :-' ,NoOfCardTurned)
         print('\nPosition OF Card 5 By Using Linear Search Algorithm :-' ,PostionOfTheCard)
         break
-    if CheckCard not in Cards:
+    if CheckCard not in str(Cards):
         print('You Entered The Invalid Card Please Check')
         break
 
