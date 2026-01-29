e03eef7b7fc2a3f563c56ae4f5ed11d59d9b10f4
Prashant Boddul <boddulprashant@gmail.com>
2026-01-29 23:06:20 +0530

    Add files via upload

---
+++ Selection Sort.py
@@ -0,0 +1,15 @@
+UsortedArray=[1,9,2,8,3,7,4,6,5,10,0]
+print('Original Array :-',UsortedArray)
+
+NoOfElementsInArr=len(UsortedArray)
+
+for i in range(NoOfElementsInArr):
+    k=i
+    for j in range(i+1,NoOfElementsInArr):
+        if UsortedArray[j]<UsortedArray[i]:
+            k=j
+            
+    UsortedArray[i],UsortedArray[k]=UsortedArray[k],UsortedArray[i]
+
+print('Original Array After Sorting:-',UsortedArray)
+
